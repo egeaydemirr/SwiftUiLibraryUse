98c0c7fba3490cc81a5612e3b9634b332ec8f5a5
Ege Aydemir <egeaydemirr97@gmail.com>
2023-01-09 23:21:37 +0300

    AddTheEffect

--- SwiftUiLibraryUse/ContentView.swift
+++ SwiftUiLibraryUse/ContentView.swift
@@ -23,6 +23,7 @@ struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
         ContentView()
         Circle().foregroundColor(.green)
+            .opacity(0.2)
     }
 }
 
