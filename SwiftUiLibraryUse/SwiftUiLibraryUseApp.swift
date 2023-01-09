57ce384b650d64afa485b527a79a3db5efd8a221
Ege Aydemir <egeaydemirr97@gmail.com>
2023-01-09 22:57:53 +0300

    first commit

---
+++ SwiftUiLibraryUse/SwiftUiLibraryUseApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUiLibraryUseApp.swift
+//  SwiftUiLibraryUse
+//
+//  Created by Ege Aydemir on 9.01.2023.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUiLibraryUseApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
